ca9625ae9656c2b15e3856659b61d688972f6ebe
andre <andre@ANDREY-PC>
2024-12-11 22:34:37 +0300

    v3

--- eigenValue/Form1.Designer.cs
+++ eigenValue/Form1.Designer.cs
@@ -39,6 +39,8 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -133,11 +135,31 @@
             this.textBox5.Size = new System.Drawing.Size(110, 20);
             this.textBox5.TabIndex = 10;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(396, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(520, 49);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(242, 184);
+            this.textBox6.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -170,6 +192,8 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox6;
     }
 }
 
--- eigenValue/Form1.cs
+++ eigenValue/Form1.cs
@@ -67,7 +67,7 @@ namespace eigenValue
                     }
                     matr[i, j] = Convert.ToDouble(subLine);
                     j++;
-                    if(j != n)
+                    if (j != n)
                     {
                         throw new Exception();
                     }
@@ -78,12 +78,12 @@ namespace eigenValue
                 MessageBox.Show("Wrong input of matrix", "Worning");
                 return;
             }
-            if(!IsSimetric(matr, n))
+            if (!IsSimetric(matr, n))
             {
                 MessageBox.Show("Matrix is not simetric", "Worning");
                 return;
             }
-            if(!IsPositiveDefinite(matr, n))
+            if (!IsPositiveDefinite(matr, n))
             {
                 MessageBox.Show("Matrix is not positive definite", "Worning");
                 return;
@@ -91,7 +91,7 @@ namespace eigenValue
             double[] x0 = new double[n];
             double[] xNext = new double[n];
             double lambda = 0;
-            for(int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
                 x0[i] = 1;
                 xNext[i] = 1;
@@ -106,7 +106,7 @@ namespace eigenValue
                 xNext = DivisionByConstant(Cmposition(matr, x0, n), Norma(Cmposition(matr, x0, n), n), n);
                 lambdaNext = SkolarCmposition(Cmposition(matr, xNext, n), xNext, n);
                 iteration++;
-            }while(Math.Abs(lambdaNext - lambda) >= eps);
+            } while (Math.Abs(lambdaNext - lambda) >= eps);
             textBox3.Text = $"{lambda}";
             textBox5.Text = $"{iteration}";
         }
@@ -116,7 +116,7 @@ namespace eigenValue
             {
                 for (int j = i+1; j < n; j++)
                 {
-                    if (matr[i,j] != matr[j, i])
+                    if (matr[i, j] != matr[j, i])
                     {
                         return false;
                     }
@@ -126,17 +126,17 @@ namespace eigenValue
         }
         public bool IsPositiveDefinite(double[,] matr, int n)
         {
-            for(int n1 = 1; n1 <= n; n1++)
+            for (int n1 = 1; n1 <= n; n1++)
             {
-                double[,] matr1 = new double[n1,n1];
-                for(int i  = 0; i < n1; i++)
+                double[,] matr1 = new double[n1, n1];
+                for (int i = 0; i < n1; i++)
                 {
-                    for(int j = 0; j < n1; j++)
+                    for (int j = 0; j < n1; j++)
                     {
-                        matr1[i,j] = matr[i,j];
+                        matr1[i, j] = matr[i, j];
                     }
                 }
-                if(Det(matr1, n1) < 0)
+                if (Det(matr1, n1) < 0)
                 {
                     return false;
                 }
@@ -145,21 +145,21 @@ namespace eigenValue
         }
         public double Det(double[,] matr, int n)
         {
-            if(n == 1)
+            if (n == 1)
             {
-                return matr[0,0];
+                return matr[0, 0];
             }
-            if(n == 2)
+            if (n == 2)
             {
-                return matr[0, 0]*matr[1,1] - matr[0,1] * matr[1,0];
+                return matr[0, 0]*matr[1, 1] - matr[0, 1] * matr[1, 0];
             }
             double res = 0;
             bool pozitiv = true;
             for (int i = 0; i < n; i++)
             {
-                double[,] matr1 = new double[n-1,n-1];
+                double[,] matr1 = new double[n-1, n-1];
                 int i1 = 0;
-                for(int j = 0; j < n; j++)
+                for (int j = 0; j < n; j++)
                 {
                     if (j==i)
                     {
@@ -168,7 +168,7 @@ namespace eigenValue
                     int j1 = 0;
                     for (int k = 1; k < n; k++)
                     {
-                        matr1[i1,j1] = matr[j,k];
+                        matr1[i1, j1] = matr[j, k];
                         j1++;
                     }
                     i1++;
@@ -181,7 +181,7 @@ namespace eigenValue
         public double[] Cmposition(double[,] matr, double[] x, int n)
         {
             double[] newX = new double[n];
-            for(int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
                 double sum = 0;
                 for (int j = 0; j < n; j++)
@@ -221,9 +221,241 @@ namespace eigenValue
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            textBox1.Text = $"5 2{Environment.NewLine}2 5";
-            textBox2.Text = "2";
+            textBox1.Text = $"2 2 -2{Environment.NewLine}2 5 -4{Environment.NewLine}-2 -4 5";
+            textBox2.Text = "3";
             textBox4.Text = "0,0001";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            int n = 0;
+            try
+            {
+                n = Convert.ToInt32(textBox2.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Input dimension", "Worning");
+                return;
+            }
+            double[,] matr = new double[n, n];
+            try
+            {
+                string[] line;
+                line = textBox1.Lines;
+                for (int i = 0; i < n; i++)
+                {
+                    int j = 0;
+                    string subLine = "";
+                    for (int g = 0; g < line[i].Length; g++)
+                    {
+                        if (line[i][g] != ' ')
+                        {
+                            subLine += line[i][g];
+                        }
+                        else
+                        {
+                            matr[i, j] = Convert.ToDouble(subLine);
+                            j++;
+                            subLine = "";
+                        }
+                    }
+                    matr[i, j] = Convert.ToDouble(subLine);
+                    j++;
+                    if (j != n)
+                    {
+                        throw new Exception();
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Wrong input of matrix", "Worning");
+                return;
+            }
+            if (!IsSimetric(matr, n))
+            {
+                MessageBox.Show("Matrix is not simetric", "Worning");
+                return;
+            }
+            if (!IsPositiveDefinite(matr, n))
+            {
+                MessageBox.Show("Matrix is not positive definite", "Worning");
+                return;
+            }
+            double[,] newA = matr;
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = i+1; j < n; j++)
+                {
+                    double Cos = CosFi(i,j,matr);
+                    double Sin = SinFi(i,j, matr);
+                    newA = A(B(matr, n, Cos, Sin, i, j), n, Cos, Sin, i, j);
+                    
+                }
+            }
+            double[,] ext = ToExtend(newA, n);
+            double[] lyamda = RunMethod(ext, n);
+
+            textBox6.Text += $"D = {D(lyamda[0], newA, n)}";
+            textBox6.AppendText(Environment.NewLine);
+            Output(lyamda, n);
+        }
+        public double[,] ToExtend(double[,] matr, int n)
+        {
+            double[,] ext = new double[n,n+1];
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    if(j == n)
+                    {
+                        ext[i,j] = 0;
+                    }
+                    else
+                    {
+                        ext[i,j] = matr[i,j];
+                    }
+                }
+            }
+            return ext;
+        }
+        //public double[,] Multiplex(double[,] a, double[,] b, int n)
+        //{
+        //    double[,] res = new double[n, n];
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        for (int j = 0; j < n; j++)
+        //        {
+        //            double sum = 0;
+        //            for (int i1 = 0; i1 < n; i1++)
+        //            {
+        //                sum += a[i, i1] * b[i1, j];
+        //            }
+        //            res[i, j] = sum;
+        //        }
+        //    }
+        //    return res;
+        //}
+        public void Output(double[,] matr, int n)
+        {
+            textBox3.Text = "";
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    textBox6.Text += $"{matr[i, j]}\t";
+                }
+                textBox6.AppendText(Environment.NewLine);
+            }
+        }
+        public void Output(double[] matr, int n)
+        {
+            textBox3.Text = "";
+            for (int i = 0; i < n; i++)
+            {
+
+                textBox6.Text += $"Lyamda{i+1} = {matr[i]}";
+                textBox6.AppendText(Environment.NewLine);
+            }
+        }
+        public double CosFi(int l, int m, double[,] a)
+        {
+            return a[l-1, l]/(Math.Sqrt(a[l-1, l]*a[l-1, l] + a[l-1, m]*a[l-1, m]));
+        }
+        public double SinFi(int l, int m, double[,] a)
+        {
+            return a[l-1, m]/(Math.Sqrt(a[l-1, l]*a[l-1, l] + a[l-1, m]*a[l-1, m]));
+        }
+        //public (double[] l1, double[] m1) B(double[,] a, int n, double Cos, double Sin, int l, int m)
+        //{
+        //    (double[] l1, double[] m1) b = (new double[n], new double[n]);
+        //    for (int j = 1; j < n; j++)
+        //    {
+        //        b.l1[j] = a[l, j]*Cos + a[m, j]*Sin;
+        //        b.m1[j] = -a[l, j]*Sin + a[m, j]*Cos;
+        //    }
+        //    return b;
+        //}
+        public double[,] B(double[,] a, int n, double Cos, double Sin, int l, int m)
+        {
+            double[,] b = new double[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for(int j = 0;j < n; j++)
+                {
+                    if (i!= l && i!= m)
+                    {
+                        b[i, j] = a[i, j];
+                    }
+                }
+            }
+            for (int j = 0; j < n; j++)
+            {
+                b[l, j] = a[l, j]*Cos + a[m, j]*Sin;
+                b[m, j] = -a[l, j]*Sin + a[m, j]*Cos;
+            }
+            return b;
+        }
+        public double[,] A(double[,] b, int n, double Cos, double Sin, int l, int m)
+        {
+            double[,] aNew = new double[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j!= l && j!= m)
+                    {
+                        aNew[i, j] = b[i, j];
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                aNew[i, l] = b[i, l]*Cos + b[i, m]*Sin;
+                aNew[i, m] = -b[i, l]*Sin + b[i, m]*Cos;
+            }
+            return aNew;
+        }
+        public double D(double x, double[,] a, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else if (n == 1)
+            {
+                return a[0,0] - x;
+            }
+            else
+            {
+                return (a[n-1,n-1] - x) * D(x, a, n-1) - a[n-1,n-2] * a[n-2,n-1]*D(x,a,n-2);
+            }
+        }
+        public double[] RunMethod(double[,] matr, int n)
+        {
+            double an1 = -matr[n-1, n-2] / matr[n-1, n-1];
+            double bn1 = matr[n-1, n] / matr[n-1, n-1];
+            double[] a = new double[n-1];
+            double[] b = new double[n-1];
+            a[n-2] = an1;
+            b[n-2] = bn1;
+            double[] xi = new double[n];
+            for (int i = n-3; i >= 0; i--)
+            {
+                a[i] = -matr[i+1, i] / (a[i+1]*matr[i+1, i+2] + matr[i+1, i+1]);
+                b[i] = (matr[i+1, n] - b[i+1]*matr[i+1, i+2]) / (a[i+1]*matr[i+1, i+2] + matr[i+1, i+1]);
+            }
+            xi[1] = (-b[0]- matr[0, n] * a[0] / matr[0, 0]) / (-1 - matr[0, 1] * a[0] / matr[0, 0]);
+            xi[0] = (matr[0, n] - matr[0, 1] * xi[1]) / matr[0, 0];
+            for (int i = 2; i < n; i++)
+            {
+                xi[i] = a[i-1] * xi[i-1] + b[i-1];
+            }
+            return xi;
         }
     }
 }
